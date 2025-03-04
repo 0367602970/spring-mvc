5038d3fba9ee4a771c6bcbb23ef0fb0f09d850b9
0367602970 <toan0230667@huce.edu.vn>
2025-03-04 20:37:37 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -2,7 +2,15 @@ package vn.hoidanit.laptopshop.domain;
 
 import org.eclipse.tags.shaded.org.apache.regexp.recompile;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String email;
     private String password;
